3906ee4e28f60e4f8a0ec62fcb599f23d33c75b0
Anjosisa <isahanjos1801@gmail.com>
2023-08-24 08:24:11 -0300

    while algoritmo

--- Projeto/algoritmo.py
+++ Projeto/algoritmo.py
@@ -1,1 +1,13 @@
 from classes import *
+
+def main():
+    s = 1
+    while s != 1:
+
+        try:
+            print("Bem vindos ao Hotel ARNI. Aqui você encontra o que desejar, mas primeiro, o que deseja fazer?\n1 - Cadastro \n2 - Reserva")
+            menu_op = int(input(""))
+
+        except Exception as erro:
+            print("Valor invalido")
+            print(erro.__class__.__name__)
