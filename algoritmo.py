e0f3b6d3902f19abbc0c565188b45cfe64f2b0c3
Arthurzinho02 <arthur.matheus0607@gmail.com>
2023-10-01 19:35:08 -0300

    update

--- algoritmo.py
+++ algoritmo.py
@@ -2,91 +2,121 @@ from classes import *
 import os
 
 def main():
-    y = 1
-    while y == 1:
+    while True:
         try:
             print("Bem vindo! Você está no Hotel ARNI.")
-            print("O que deseja?\n[1] Cadastrar-se\n[2] Alugar quartos\n[3] Cancelamento de reserva\n[4] Visualizar Reservas\n[5] Sair")
-            escolha = input("- ")
+            print("O que deseja?\n[1] Login \n[2] Cadastrar \n[3] Sair")
+            escolha = int(input(">> "))
             os.system("cls")
+            match escolha:
+                case 1:
+                    print("=== Login ===")
+                    print("Insira os dados para login:")
+                    emaillogin = input("E-mail: ")
+                    senhalogin = input("Senha: ")
+                    os.system("cls")
 
-            if escolha == "1":
-                print("=== Área de Cadastro ===")
-                nomecadastro = input("Nome de usuário: ")
-                emailcadastro = input("E-mail: ")
-                senhacadastro = input("Senha: ")
-                recepcionista.Cadastrar(nomecadastro, emailcadastro, senhacadastro)
-                os.system("cls")
-                print("Parabéns! Você foi cadastrado com sucesso. ")
-                print("As seguintes informações de sua conta foram salvas:")
-                print(f"Usuário: {nomecadastro}\nE-mail: {emailcadastro}\nSenha: {senhacadastro}")
-                os.system("pause")
-                os.system("cls")
+                    match recepcionista.login(emaillogin, senhalogin):
+                        case 1:
+                                while True:
+                                    print(f"=== Bem vindo {recepcionista.getNomeC(emaillogin)}===")
+                                    print("Deseja: \n[1] Vizualizar quartos \n[2] Alugar \n[3] Cancelar Reserva \n[4] Ver reservas \n[5] Sair")
+                                    opção = int(input(">> "))
+                                    os.system("cls")
 
-            elif escolha == "2":
-                print("=== Alugar Quartos ===")
-                print("Insira os dados para login:")
-                emaillogin = input("E-mail: ")
-                senhalogin = input("Senha: ")
-                os.system("cls")
+                                    match opção:
+                                        case 1:
+                                            print("=== Quartos ===")
+                                            recepcionista.Visualizar()
+                                            os.system("pause")
+                                            os.system("cls")
 
-                if recepcionista.login(emaillogin, senhalogin) == 1:
-                    print("Bem vindo!\nQuartos Disponíveis:\n")
-                    recepcionista.Visualizar()
+                                        case 2:
+                                            print("=== Bem vindo ===\nQuartos Disponíveis:\n")
+                                            recepcionista.Visualizar()
 
-                    x = input("\nQual quarto deseja alugar?\n- ")
+                                            x = int(input("\nQual quarto deseja alugar?\n>> "))
+                                            match x:
 
-                    if x == "1":
-                        recepcionista.LuxoCadastro(emaillogin)
-                        os.system("pause")
-                        os.system("cls")
+                                                case 1:
+                                                    recepcionista.LuxoCadastro(emaillogin)
+                                                    os.system("pause")
+                                                    os.system("cls")
 
-                    elif x == "2":
-                        recepcionista.MasterCadastro(emaillogin)
-                        os.system("pause")
-                        os.system("cls")
+                                                case 2:
+                                                    recepcionista.MasterCadastro(emaillogin)
+                                                    os.system("pause")
+                                                    os.system("cls")
 
-                    elif x == "3":
-                        recepcionista.SimplesCadastro(emaillogin)
-                        os.system("pause")
-                        os.system("cls")
+                                                case 3:
+                                                    recepcionista.SimplesCadastro(emaillogin)
+                                                    os.system("pause")
+                                                    os.system("cls")
 
-                    elif x == "4":
-                        recepcionista.SimplesCasalCadastro(emaillogin)
-                        os.system("pause")
-                        os.system("cls")
+                                                case 4:
+                                                    recepcionista.SimplesCasalCadastro(emaillogin)
+                                                    os.system("pause")
+                                                    os.system("cls")
 
-                    elif x == "5":
-                        recepcionista.DuoCadastro(emaillogin)
-                        os.system("pause")
-                        os.system("cls")
+                                                case 5:
+                                                    recepcionista.DuoCadastro(emaillogin)
+                                                    os.system("pause")
+                                                    os.system("cls")
 
-                    elif x == "6":
-                        recepcionista.DuoCasalCadastro(emaillogin)
-                        os.system("pause")
-                        os.system("cls")
+                                                case 6:
+                                                    recepcionista.DuoCasalCadastro(emaillogin)
+                                                    os.system("pause")
+                                                    os.system("cls")
 
-                    else:
-                        print("O quarto não existe.")
-                    
-                elif recepcionista.login(emaillogin, senhalogin) == 2:
-                    print("Cliente não existe ou não foi encontrado.")
+                                                case _:
+                                                    print("O quarto não existe.")
+                                                    os.system("pause")
+                                                    os.system("cls")
+                                    
+                                        case 3:
+                                            print ("=== Cancelamento de Reserva ===") 
+                                            recepcionista.Quartos()
+                                            vetor = int(input("Digite o índice do quarto a ser excluido: "))
+                                            recepcionista.Cancelar(emaillogin, vetor)
+                                            print("Quarto excluido")
+                                            os.system("pause")
+                                            os.system("cls")
+
+                                        case 4:
+                                            print ("=== Visualizar Reservas ===")
+                                            recepcionista.ClienteVisualizar(emaillogin)
+                                            os.system("pause")
+                                            os.system("cls")
+
+                                        case 5:
+                                            break
+
+                                        case _:
+                                            print("Inválido")
+                                            os.system("pause")
+                                            os.system("cls")
+                        case _:
+                            print("Senha ou email incorretos")
+                case 2:
+                    print("=== Área de Cadastro ===")
+                    nomecadastro = input("Nome de usuário: ")
+                    emailcadastro = input("E-mail: ")
+                    senhacadastro = input("Senha: ")
+                    recepcionista.Cadastrar(nomecadastro, emailcadastro, senhacadastro)
+                    os.system("cls")
+                    print("Parabéns! Você foi cadastrado com sucesso. ")
+                    print("As seguintes informações de sua conta foram salvas:")
+                    print(f"Usuário: {nomecadastro}\nE-mail: {emailcadastro}\nSenha: {senhacadastro}")
                     os.system("pause")
                     os.system("cls")
 
-            elif escolha == "3":
-                print ("=== Cancelamento de Reserva ===") 
-                print ("Insira os seus dados para realizar o cancelamento:")
-                emaillogin = input("E-mail: ")
-                senhalogin = input("Senha: ")
-                recepcionista.Cancelar(emaillogin, senhalogin)
+                case 3:
+                    break
 
-            elif escolha == "4":
-                print ("=== Visualizar Reservas ===")
-                recepcionista.ClienteVisualizar()
-
-            elif escolha == "5":
-                y = 0
+                case _:
+                    print("Inválido")
+                    os.system("pause")
+                    os.system("cls")
                 
         except Exception as erro:
             print(f"O erro é: {erro.__class__.__name__}")
--- classes.py
+++ classes.py
@@ -3,6 +3,15 @@ class Cliente:
         self.nome = nome
         self.email = email
         self.senha = senha
+
+    def getNome(self):
+        return self.nome
+    
+    def getEmail(self):
+        return self.email
+    
+    def getSenha(self):
+        return self.senha
     
 class Hotel:
     def __init__(self):
@@ -15,6 +24,12 @@ class Hotel:
         self.limduo = 3
         self.limduocasal = 3
         self.quartos = ["Luxo", "Master", "Simples", "Simples Casal", "Duo", "Duo Casal"]
+
+    def Quartos(self):
+        cont = 0
+        for i in self.quartos:
+            cont += 1
+            print(f"{cont} - {i}")
     
     def Visualizar(self):
         print(f"[1]Luxo: {self.limluxo} \n[2]Master: {self.limmaster} \n[3]Simples: {self.limsimples} \n[4]Simples Casal: {self.limsimplescasal} \n[5]Duplo Simples: {self.limduo} \n[6]Duplo Casal: {self.limduocasal}")
@@ -25,34 +40,39 @@ class Hotel:
 
     def login(self, email, senha):
         for cliente in self.nomes:
-            if cliente.email == email  and cliente.senha == senha:
+            if cliente.getEmail() == email  and cliente.getSenha() == senha:
                 return 1
-            else:
-                return 2
+            
+    def getNomeC(self, email):
+        for cliente in self.nomes:
+            if cliente.getEmail() == email:
+                return cliente.getNome()
+    
 
 class Quartos(Hotel):
-    def ClienteVisualizar(self, nome):
+    def ClienteVisualizar(self, email):
+        cont = 0
         for chave, valor in self.clientes.items():
-            if chave.nome == nome:
-                print(f"{chave.nome} | {valor}")
-            else:
-                print("Nome não encontrado")
+            if chave.getEmail() == email:
+                cont += 1
+                print(f"{cont} - {valor}")
+            
         
-    def Cancelar(self, email, quarto):
+    def Cancelar(self, email, vetorQ):
         for chave, valor in self.clientes.items():
-            if chave.email == email:
-                if valor == quarto:
-                    self.clientes.pop(chave)
-                    print(f"Quarto {quarto} excluido")
+            if chave.getEmail() == email and valor == self.quartos[vetorQ - 1]:
+                self.clientes.pop(chave)
+                break
+                    
                 
         
     def LuxoCadastro(self, email):
         if self.limluxo <= 2 and self.limluxo > 0:
             for cliente in self.nomes:
-                if cliente.email == email:
+                if cliente.getEmail() == email:
                     self.clientes[cliente] = self.quartos[0]
                     self.limluxo = self.limluxo - 1
-                    print(f"Quarto de Luxo alugado por {cliente.nome}")
+                    print(f"Quarto de Luxo alugado por {cliente.getNome()}")
                 else:
                     print("Cliente não encontrado")
         else:
@@ -61,10 +81,10 @@ class Quartos(Hotel):
     def MasterCadastro(self, email):
         if self.limmaster <= 2 and self.limmaster > 0:
             for cliente in self.nomes:
-                if cliente.email == email:
+                if cliente.getEmail() == email:
                     self.clientes[cliente] = self.quartos[1]
                     self.limmaster = self.limmaster - 1
-                    print(f"Quarto Master alugado por {cliente.nome}")
+                    print(f"Quarto Master alugado por {cliente.getNome()}")
                 else:
                     print("Cliente não encontrado")
         else:
@@ -73,10 +93,10 @@ class Quartos(Hotel):
     def SimplesCadastro(self, email):
         if self.limsimples <= 3 and self.limsimples > 0:
             for cliente in self.nomes:
-                if cliente.email == email:
+                if cliente.getEmail() == email:
                     self.clientes[cliente] = self.quartos[2]
                     self.limsimples = self.limsimples - 1
-                    print(f"Quarto simples alugado por {cliente.nome}")
+                    print(f"Quarto simples alugado por {cliente.getNome()}")
                 else:
                     print("Cliente não encontrado")
         else:
@@ -85,10 +105,10 @@ class Quartos(Hotel):
     def SimplesCasalCadastro(self, email):
         if self.limsimplescasal <= 3 and self.limsimplescasal > 0:
             for cliente in self.nomes:
-                if cliente.email == email:
+                if cliente.getEmail() == email:
                     self.clientes[cliente] = self.quartos[3]
                     self.limsimplescasal = self.limsimplescasal - 1
-                    print(f"Quarto Simples de Casal alugado por {cliente.nome}")
+                    print(f"Quarto Simples de Casal alugado por {cliente.getNome()}")
                 else:
                     print("Cliente não encontrado")
         else:
@@ -97,10 +117,10 @@ class Quartos(Hotel):
     def DuoCadastro(self, email):
         if self.limduo <= 3 and self.limduo > 0:
             for cliente in self.nomes:
-                if cliente.email == email:
+                if cliente.getEmail() == email:
                     self.clientes[cliente] = self.quartos[4]
                     self.limduo = self.limduo - 1
-                    print(f"Quarto duplo alugado por {cliente.nome}")
+                    print(f"Quarto duplo alugado por {cliente.getNome()}")
                 else:
                     print("Cliente não encontrado")
         else:
@@ -109,10 +129,10 @@ class Quartos(Hotel):
     def DuoCasalCadastro(self, email):
         if self.limduocasal <= 3 and self.limduocasal > 0:
             for cliente in self.nomes:
-                if cliente.email == email:
+                if cliente.getEmail() == email:
                     self.clientes[cliente] = self.quartos[5]
                     self.limduocasal = self.limduo - 1
-                    print(f"Quarto Duo de Casal alugado por {cliente.nome}")
+                    print(f"Quarto Duo de Casal alugado por {cliente.getNome()}")
                 else:
                     print("Cliente não encontrado")
         else:
